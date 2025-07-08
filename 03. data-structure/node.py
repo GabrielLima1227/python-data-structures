b30cb80d7f7c6929f08d24beadb5a3d0a6826023
Gabriel de Lima Sousa <gablimax@hotmail.com>
2025-07-08 15:41:40 -0300

    feat:Learning about Linked Lists

---
+++ 03. data-structure/node.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    def set_next(self, node):
+        self.next = node
+
+    def __repr__(self):
+        return self.val
+
+
+fruits = Node("Banana")
+fruits.set_next(Node("grape"))
